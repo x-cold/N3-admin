b6277d1b0f84245e54fc024f2a05c79df1da842f
x-cold <lxstart@outlook.com>
2018-06-04 10:33:02 +0800

    chore: fix unsecure deps

---
+++ docs/static/js/0.e00d47a2b5d1994ff1b4.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,1],{302:function(e,t,n){var s=n(6)(null,n(309),null,null);e.exports=s.exports},309:function(e,t){e.exports={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("section",{staticClass:"test-page"},[e._v("\n  test page\n")])},staticRenderFns:[]}}});
+//# sourceMappingURL=0.e00d47a2b5d1994ff1b4.js.map
